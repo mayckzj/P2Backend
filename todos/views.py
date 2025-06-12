d098c2130276822b75f3c07c351fd79ae0d5a777
denis <denisnascimento3@gmail.com>
2025-06-12 19:43:29 -0300

    Implemente a funcionalidade para adicionar tarefas com CreateView. (DONE!)

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Todo
 from django.urls import reverse_lazy
 
-
-
 class TodoListView(ListView):
     model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title','deadline']
+    success_url = reverse_lazy('todo_list')
